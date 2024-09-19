4564832f7179e4c7dfff8c1c38139b4bbc83cf3f
Vamshi51282 <135943611+Vamshi51282@users.noreply.github.com>
2024-09-19 18:14:04 +0530

    Add files via upload

---
+++ 18SepTask.js
@@ -0,0 +1,20 @@
+var name="Vamshi";
+var age=22;
+var job="Software-Developer"
+var sal=999;
+
+console.log(name);
+console.log(age);
+console.log(job);
+console.log(sal);
+
+
+var details={
+    Name:"Vamshi",
+    age : 22,
+    job : "Software-Developer"
+}
+console.log(details.job);
+
+var info = [{ father: "SrinivasReddy", Occupation:{ work: "farmer"}}, {mother: "Bhagya", occupation:{ work: "Homemaker"}}]
+console.log(info);
